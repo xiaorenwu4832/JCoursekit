ec10a9081b375b1fd61d98392b1ea1aff5d07ffd
Hony <779509583@qq.com>
2021-03-09 11:58:14 +0800

    V0.0.1

--- CourseKit.ts
+++ CourseKit.ts
@@ -10,7 +10,7 @@
 /*
 liquid block
 */
-//% weight=80 color=#66FFFF icon="\uf043" block="liquid"
+//% weight=80 color=#66FFFF icon="\uf043" block="液位检测"
 namespace Liquid {
     let pin = AnalogPin.P0
     let volt = 0;
@@ -22,7 +22,7 @@ namespace Liquid {
      * @param pin_arg pin at which the Liquid Level Senor AOUT line is connected;
      */
     //% blockId=Liquid_setPin
-    //% block="set liquid pin |%pinarg|"
+    //% block="设置液位模拟引脚 |%pinarg|"
     //% weight = 85
     export function setPin(pin_arg: AnalogPin): void {
         pin = pin_arg;
@@ -33,7 +33,7 @@ namespace Liquid {
      * Return the adc value from the AnalogPin;
      */
     //% blockId=Liquid_getADCValue
-    //% block="get liquid adc value"
+    //% block="获取液位ADC值"
     //% weight = 75
     export function getADCValue(): number {
         adc = pins.analogReadPin(pin);
@@ -44,7 +44,7 @@ namespace Liquid {
      * Return the volt value from the AnalogPin;
      */
     //% blockId=Liquid_getVoltValue
-    //% block="get liquid volt value"
+    //% block="获取液位电压值"
     //% weight = 65
     export function getVoltValue(): number {
         adc = pins.analogReadPin(pin);
@@ -58,7 +58,7 @@ namespace Liquid {
 /*
 sound block
 */
-//% weight=70 color=#CC6633 icon="\uf001" block="sound"
+//% weight=70 color=#CC6633 icon="\uf001" block="麦克风"
 namespace Sound {
     let Apin = AnalogPin.P0;
     let Dpin = DigitalPin.P1;
@@ -73,7 +73,7 @@ namespace Sound {
      * @param a_pin_arg Sound Sensor analog connected pin;
      */
     //% blockId=Sound_setApin
-    //% block="模拟引脚连接在 |%a_pin_arg|"
+    //% block="设置麦克风模拟引脚 |%a_pin_arg|"
     //% weight = 85
     export function setApin(a_pin_arg: AnalogPin): void {
         Apin = a_pin_arg;
@@ -85,7 +85,7 @@ namespace Sound {
      * @param d_pin_arg Sound Sensor digital connected pin;
      */
     //% blockId=Sound_setDpin
-    //% block="数字引脚连接在|%d_pin_arg|"
+    //% block="设置麦克风数字引脚|%d_pin_arg|"
     //% weight = 75
     export function setDpin(d_pin_arg: DigitalPin): void {
         Dpin = d_pin_arg;
@@ -97,7 +97,7 @@ namespace Sound {
      * Return the sound sensor adc value from the AnalogPin;
      */
     //% blockId=Sound_getADCValue
-    //% block="获取ADC值"
+    //% block="获取麦克风ADC值"
     //% weight = 65
     export function getADCValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -108,7 +108,7 @@ namespace Sound {
      * Return the sound sensor volt value from the AnalogPin;
      */
     //% blockId=Sound_getVoltValue
-    //% block="获取电压值"
+    //% block="获取麦克风电压值"
     //% weight = 55
     export function getVoltValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -120,7 +120,7 @@ namespace Sound {
      * Return the sound sensor volt value from the DigitalPin;
      */
     //% blockId=Sound_getNoise
-    //% block="get whether noise is loud"
+    //% block="是否处于在噪声中"
     //% weight = 45
     export function getNoise(): number {
         noise = pins.digitalReadPin(Dpin);
@@ -140,7 +140,7 @@ namespace Sound {
 /*
 infrared  block
 */
-//% weight=60 color=#FF0000 icon="\uf0eb" block="infrared"
+//% weight=60 color=#FF0000 icon="\uf0eb" block="红外避障"
 namespace Infrared {
     let Apin = AnalogPin.P0;
     let Dpin = DigitalPin.P1;
@@ -155,7 +155,7 @@ namespace Infrared {
      * @param a_pin_arg Infrared Sensor analog connected pin;
      */
     //% blockId=Infrared_setApin
-    //% block="infrared sensor analog pin connect to |%a_pin_arg|"
+    //% block="设置红外模拟引脚 |%a_pin_arg|"
     //% weight = 85
     export function setApin(a_pin_arg: AnalogPin): void {
         Apin = a_pin_arg;
@@ -167,7 +167,7 @@ namespace Infrared {
      * @param d_pin_arg Infrared Sensor digital connected pin;
      */
     //% blockId=Infrared_setDpin
-    //% block="infrared sensor digital pin connect to |%d_pin_arg|"
+    //% block="设置红外数字引脚 |%d_pin_arg|"
     //% weight = 75
     export function setDpin(d_pin_arg: DigitalPin): void {
         Dpin = d_pin_arg;
@@ -179,7 +179,7 @@ namespace Infrared {
      * Return the infrared sensor adc value from the AnalogPin;
      */
     //% blockId=Infrared_getADCValue
-    //% block="get infrared adc value"
+    //% block="获得红外ADC值"
     //% weight = 65
     export function getADCValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -190,7 +190,7 @@ namespace Infrared {
      * Return the infrared sensor volt value from the AnalogPin;
      */
     //% blockId=Infrared_getVoltValue
-    //% block="get infrared volt value"
+    //% block="获得红外电压值"
     //% weight = 55
     export function getVoltValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -202,7 +202,7 @@ namespace Infrared {
      * Return the infrared sensor level value from the DigitalPin;
      */
     //% blockId=Infrared_getClose
-    //% block="get whether infrared near obstacles"
+    //% block="检测到前方是否有障碍"
     //% weight = 45
     export function getClose(): number {
         close = pins.digitalReadPin(Dpin);
@@ -302,7 +302,7 @@ namespace Gas {
 /*
 flame block
 */
-//% weight=40 color=#FF6633 icon="\uf06d" block="flame"
+//% weight=40 color=#FF6633 icon="\uf06d" block="火焰检测"
 namespace Flame {
     let Apin = AnalogPin.P0;
     let Dpin = DigitalPin.P1;
@@ -317,7 +317,7 @@ namespace Flame {
      * @param a_pin_arg Flame Sensor analog connected pin;
      */
     //% blockId=Flame_setApin
-    //% block="flame sensor analog pin connect to |%a_pin_arg|"
+    //% block="设置火焰检测模拟引脚" |%a_pin_arg|"
     //% weight = 85
     export function setApin(a_pin_arg: AnalogPin): void {
         Apin = a_pin_arg;
@@ -329,7 +329,7 @@ namespace Flame {
      * @param d_pin_arg Flame Sensor digital connected pin;
      */
     //% blockId=Flame_setDpin
-    //% block="flame sensor digital pin connect to |%d_pin_arg|"
+    //% block="设置火焰检测数字引脚|%d_pin_arg|"
     //% weight = 75
     export function setDpin(d_pin_arg: DigitalPin): void {
         Dpin = d_pin_arg;
@@ -341,7 +341,7 @@ namespace Flame {
      * Return the Flame sensor adc value from the AnalogPin;
      */
     //% blockId=Flame_getADCValue
-    //% block="get flame adc value"
+    //% block="获取火焰检测ADC值"
     //% weight = 65
     export function getADCValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -352,7 +352,7 @@ namespace Flame {
      * Return the flame sensor volt value from the AnalogPin;
      */
     //% blockId=Flame_getVoltValue
-    //% block="get flame volt value"
+    //% block="获取火焰检测电压值"
     //% weight = 55
     export function getVoltValue(): number {
         adc = pins.analogReadPin(Apin);
@@ -364,7 +364,7 @@ namespace Flame {
      * Return the flame sensor level value from the DigitalPin;
      */
     //% blockId=Flame_getPolute
-    //% block="get whether far from fire"
+    //% block="是否远离火焰"
     //% weight = 45
     export function getFar(): number {
         far = pins.digitalReadPin(Dpin);
@@ -378,7 +378,7 @@ namespace Flame {
 /*
 moisture block
 */
-//% weight=30 color=#FF9900 icon="\uf275" block="moisture"
+//% weight=30 color=#FF9900 icon="\uf275" block="土壤湿度"
 namespace Moisture {
     let pin = AnalogPin.P0
     let volt = 0;
@@ -390,7 +390,7 @@ namespace Moisture {
      * @param pin_arg pin at which the Moisture Senor AOUT line is connected;
      */
     //% blockId=Moisture_setPin
-    //% block="set moisture pin |%pinarg|"
+    //% block="设置土壤湿度检测引脚 |%pinarg|"
     //% weight = 85
     export function setPin(pin_arg: AnalogPin): void {
         pin = pin_arg;
@@ -401,7 +401,7 @@ namespace Moisture {
      * Return the adc value from the AnalogPin;
      */
     //% blockId=Moisture_getADCValue
-    //% block="get moisture adc value"
+    //% block="获得土壤检测ADC值"
     //% weight = 75
     export function getADCValue(): number {
         adc = pins.analogReadPin(pin);
@@ -412,7 +412,7 @@ namespace Moisture {
      * Return the volt value from the AnalogPin;
      */
     //% blockId=Moisture_getVoltValue
-    //% block="get moisture volt value"
+    //% block="获得土壤检测电压值"
     //% weight = 65
     export function getVoltValue(): number {
         adc = pins.analogReadPin(pin);
@@ -420,205 +420,6 @@ namespace Moisture {
         return volt;
     }
 }
-
-
-
-
-
-
-
-/*
-mcp23017 block
-*/
-//% weight=20 color=#9900CC icon="\uf13d" block="mcp23017"
-namespace MCP23017 {
-
-
-    const MCP23017_IODIRA = 0x00
-    const MCP23017_IPOLA = 0x02
-    const MCP23017_GPINTENA = 0x04
-    const MCP23017_DEFVALA = 0x06
-    const MCP23017_INTCONA = 0x08
-    const MCP23017_IOCONA = 0x0A
-    const MCP23017_GPPUA = 0x0C
-    const MCP23017_INTFA = 0x0E
-    const MCP23017_INTCAPA = 0x10
-    const MCP23017_GPIOA = 0x12
-    const MCP23017_OLATA = 0x14
-
-    const MCP23017_IODIRB = 0x01
-    const MCP23017_IPOLB = 0x03
-    const MCP23017_GPINTENB = 0x05
-    const MCP23017_DEFVALB = 0x07
-    const MCP23017_INTCONB = 0x09
-    const MCP23017_IOCONB = 0x0B
-    const MCP23017_GPPUB = 0x0D
-    const MCP23017_INTFB = 0x0F
-    const MCP23017_INTCAPB = 0x11
-    const MCP23017_GPIOB = 0x13
-    const MCP23017_OLATB = 0x15
-
-
-    export enum REGISTER {
-        IODIRA = 0x00,
-        IODIRB = 0x01,
-        IPOLA = 0x02,
-        IPOLB = 0x03,
-        GPINTENA = 0x04,
-        GPINTENB = 0x05,
-        DEFVALA = 0x06,
-        DEFVALB = 0x07,
-        INTCONA = 0x08,
-        INTCONB = 0x09,
-        IOCONA = 0x0A,
-        IOCONB = 0x0B,
-        GPPUA = 0x0C,
-        GPPUB = 0x0D,
-        INTFA = 0x0E,
-        INTFB = 0x0F,
-        INTCAPA = 0x10,
-        INTCAPB = 0x11,
-        GPIOA = 0x12,
-        GPIOB = 0x13,
-        OLATA = 0x14,
-        OLATB = 0x15
-    }
-
-    export enum PIN {
-        A = 0,
-        B = 1
-    }
-
-
-    export enum MCP23017_I2C_ADDRESS {
-        ADDR_0x20 = 0x20,
-        ADDR_0x21 = 0x21,
-        ADDR_0x22 = 0x22,
-        ADDR_0x23 = 0x23,
-        ADDR_0x24 = 0x24,
-        ADDR_0x25 = 0x25,
-        ADDR_0x26 = 0x26,
-        ADDR_0x27 = 0x27
-    }
-
-    let initialized = false
-    let MCP23017_ADDRESS = MCP23017_I2C_ADDRESS.ADDR_0x27
-
-
-    function i2cWrite(addr: number, reg: number, value: number): void {
-        let buf = pins.createBuffer(2);
-        buf[0] = reg;
-        buf[1] = value;
-        pins.i2cWriteBuffer(addr, buf);
-    }
-
-    function i2cRead(addr: number, reg: number): number {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-
-    function initMCP23017(): void {
-        for (let regAddr = 0; regAddr < 22; regAddr++) {
-            if (regAddr == 0 || regAddr == 1) {
-                i2cWrite(MCP23017_ADDRESS, regAddr, 0xFF);
-            }
-            else {
-                i2cWrite(MCP23017_ADDRESS, regAddr, 0x00);
-            }
-        }
-
-        initialized = true;
-    }
-
-
-	/**
-	 * WriteData to PinA or PinB
-	 * @param addr [0-7] choose address; eg: MCP23017.MCP23017_I2C_ADDRESS.ADDR_0x27
-	*/
-    //% blockId="MCP23017_setAddress"
-    //% block="set mcp23017 device address %addr"
-    //% weight=85
-    export function setAddress(addr: MCP23017_I2C_ADDRESS) {
-        MCP23017_ADDRESS = addr
-    }
-
-
-
-    /**
-	 *Read data from MCP23017 register
-	 * @param reg [0-21] register of mcp23017; eg: 0, 15, 23
-	*/
-    //% blockId=MCP23017_readReg 
-    //% block="read register |%reg| data"
-    //% weight=75
-    export function readReg(reg: REGISTER): number {
-        let val = i2cRead(MCP23017_ADDRESS, reg);
-        return val;
-    }
-
-
-	/**
-	 * WriteData to MCP23017 PinAx or PinBx
-	 * @param pin [0-1] choose PinA or PinB; eg: 0, 1
-     * @param value [0-255] the pin value; eg: 128, 0, 255
-	*/
-    //% blockId=MCP23017_writePin
-    //% block="set P|%pin| value |%value|"
-    //% weight=65
-    //% value.min=0 value.max=255
-    export function writePin(pin: PIN, value: number): void {
-        if (!initialized) {
-            initMCP23017();
-        }
-        if (pin == 0) {
-            i2cWrite(MCP23017_ADDRESS, MCP23017_IODIRA, 0x00);
-            i2cWrite(MCP23017_ADDRESS, MCP23017_GPIOA, value);
-        }
-        else {
-            i2cWrite(MCP23017_ADDRESS, MCP23017_IODIRB, 0x00);
-            i2cWrite(MCP23017_ADDRESS, MCP23017_GPIOB, value);
-        }
-    }
-
-	/**
-	 *ReadData From PinA or PinB 
-	 * @param pin [0-1] choose PinA or PinB; eg: 0, 1
-	*/
-    //% blockId=MCP23017_readPin
-    //% block="read data from P|%pin|"
-    //% weight=55
-    export function readPin(pin: PIN): number {
-        if (!initialized) {
-            initMCP23017();
-        }
-        if (pin == 0) {
-
-            //configue all PinA input
-            i2cWrite(MCP23017_ADDRESS, MCP23017_IODIRA, 0xFF);
-
-            //configue all PinA pullUP
-            i2cWrite(MCP23017_ADDRESS, MCP23017_GPPUA, 0xFF);
-
-            let val = i2cRead(MCP23017_ADDRESS, MCP23017_GPIOA);
-
-            return val;
-        }
-        else {
-
-            //configue all PinB input
-            i2cWrite(MCP23017_ADDRESS, MCP23017_IODIRB, 0xFF);
-
-            //configue all PinB pullUP
-            i2cWrite(MCP23017_ADDRESS, MCP23017_GPPUB, 0xFF);
-
-            let val = i2cRead(MCP23017_ADDRESS, MCP23017_GPIOB);
-
-            return val;
-        }
-    }
-}
-
 
 
 
@@ -738,7 +539,7 @@ namespace BME280 {
      * get pressure(Pa)
      */
     //% blockId="BME280_pressure" 
-    //% block="pressure"
+    //% block="获取气压"
     //% weight=80 
     //% blockGap=8
     export function pressure(): number {
@@ -750,7 +551,7 @@ namespace BME280 {
      * get temperature(*C)
      */
     //% blockId="BME280_temperature" 
-    //% block="temperature"
+    //% block="获取温度"
     //% weight=80 
     //% blockGap=8
     export function temperature(): number {
@@ -762,7 +563,7 @@ namespace BME280 {
      * get humidity(%RH)
      */
     //% blockId="BME280_hunidity" 
-    //% block="humidity"
+    //% block="获取湿度"
     //% weight=80 
     //% blockGap=8
     export function hunidity(): number {
@@ -774,7 +575,7 @@ namespace BME280 {
      * power on
      */
     //% blockId="BME280_forceMode" 
-    //% block="force mode on"
+    //% block="运行模式"
     //% weight=61 
     //% blockGap=8
     //% advanced=true
@@ -786,7 +587,7 @@ namespace BME280 {
      * power off
      */
     //% blockId="BME280_normalMode" 
-    //% block="normal mode on"
+    //% block="低功耗模式"
     //% weight=60 
     //% blockGap=8
     //% advanced=true
@@ -799,7 +600,7 @@ namespace BME280 {
      * @param addr [6-7] choose address; eg: BME280.BME280_I2C_ADDRESS.ADDR_0x77
      */
     //% blockId="BME280_setAddress" 
-    //% block="set bme280 device address %addr"
+    //% block="设置BME280地址 %addr"
     //% weight=50 
     //% blockGap=8
     export function setAddress(addr: BME280_I2C_ADDRESS) {
